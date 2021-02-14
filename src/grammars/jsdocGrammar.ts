601a2cd08260739baeefb567741c4274ca42f367
Simon Seyock <seyock@terrestris.de>
2021-02-14 14:38:59 +0100

    array brackets

--- src/grammars/jsdocGrammar.ts
+++ src/grammars/jsdocGrammar.ts
@@ -2,6 +2,7 @@ import { Grammar } from './Grammar'
 import { closureGrammar } from './closureGrammar'
 import { SymbolParslet } from '../parslets/SymbolParslet'
 import { ClassPathParslet } from '../parslets/ClassPathParslet'
+import { ArrayBracketsParslet } from '../parslets/ArrayBracketsParslet'
 
 export const jsdocGrammar: Grammar = () => {
   const {
@@ -14,7 +15,8 @@ export const jsdocGrammar: Grammar = () => {
     infixParslets: [
       ...infixParslets,
       new SymbolParslet(),
-      new ClassPathParslet()
+      new ClassPathParslet(),
+      new ArrayBracketsParslet()
     ]
   }
 }
---
+++ src/parslets/ArrayBracketsParslet.ts
@@ -0,0 +1,30 @@
+import { InfixParslet } from './Parslet'
+import { TokenType } from '../lexer/Token'
+import { ParserEngine } from '../ParserEngine'
+import { ParseResult } from '../ParseResult'
+import { Precedence } from './Precedence'
+
+export class ArrayBracketsParslet implements InfixParslet {
+  accepts (type: TokenType, next: TokenType): boolean {
+    return type === '[' && next === ']'
+  }
+
+  getPrecedence (): number {
+    return Precedence.POSTFIX
+  }
+
+  parse (parser: ParserEngine, left: ParseResult): ParseResult {
+    parser.consume('[')
+    parser.consume(']')
+    return {
+      type: 'GENERIC',
+      subject: {
+        type: 'NAME',
+        name: 'Array'
+      },
+      objects: [
+        left
+      ]
+    }
+  }
+}
