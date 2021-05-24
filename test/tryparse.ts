ff31116afe76df5ac67c4f6b037b7d9cb86e8e07
Simon Seyock <seyock@terrestris.de>
2021-05-24 21:05:00 +0200

    test: add tryparse test

---
+++ test/tryparse.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+import { tryParse } from '../src/parse'
+
+describe('tryParse', () => {
+  it('should parse a valid expression in jsdoc', () => {
+    expect(() => {
+      tryParse('function(a, ...[b])')
+    }).not.to.throw()
+  })
+
+  it('should throw an error vor invalid type syntax in any type', () => {
+    expect(() => {
+      tryParse('A<')
+    }).to.throw()
+  })
+})
