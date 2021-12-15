d3abdd0deb7fda66a5ff3320a3638836a47872d5
simonseyock <simonseyock@googlemail.com>
2021-12-15 13:41:07 +0100

    docs: fixture

--- test/fixtures/Fixture.ts
+++ test/fixtures/Fixture.ts
@@ -13,6 +13,13 @@ type CatharsisMode = 'jsdoc' | 'closure'
 type CompareMode = ParseMode | 'fail' | 'differ'
 
 export interface Fixture {
+  /**
+   * The input that should be parsed
+   */
+  input: string
+  /**
+   * The {@link ParseMode}s that the expression is expected to get parsed in. In all other modes it is expected to fail.
+   */
   modes: ParseMode[]
   jtp?: {
     [K in JtpMode]: CompareMode
@@ -20,11 +27,21 @@ export interface Fixture {
   catharsis?: {
     [K in CatharsisMode]: CompareMode
   }
+  /**
+   * The expected parse result object. If you expect different parse results for different parse modes please use
+   * `diffExpected`.
+   */
   expected?: TerminalResult
+  /**
+   * The expected parse results objects for different modes. If a mode is included in `modes` and as a key of
+   * `diffExpected` the object in `diffExpected` is used over the result in `expected`.
+   */
   diffExpected?: {
     [K in ParseMode]?: TerminalResult
   }
-  input: string
+  /**
+   * If the stringified output differs from the input it can be provided here. These are mostly whitespace differences.
+   */
   stringified?: string
 }
 
@@ -97,6 +114,9 @@ function compareJtp (mode: JtpMode, compareMode: CompareMode, results: Results, 
   }
 }
 
+/**
+ * Function to run all relevant tests for a {@link Fixture}.
+ */
 export function testFixture (fixture: Fixture): void {
   const results: Results = {
     closure: testParser('closure', fixture),
