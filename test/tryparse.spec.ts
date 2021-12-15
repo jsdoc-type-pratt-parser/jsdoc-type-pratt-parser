98f456b2efb0284dc4e2f7842c8de2b0651446c0
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2021-12-14 20:11:02 -0700

    Add test cases that should not throw

--- test/tryparse.spec.ts
+++ test/tryparse.spec.ts
@@ -6,6 +6,24 @@ describe('tryParse', () => {
     expect(() => {
       tryParse('function(a, ...[b])')
     }).not.to.throw()
+    expect(() => {
+      tryParse('foo')
+    }).not.to.throw()
+    expect(() => {
+      tryParse('params.foo')
+    }).not.to.throw()
+    expect(() => {
+      tryParse('readOnly')
+    }).not.to.throw()
+    expect(() => {
+      tryParse('params.readOnly')
+    }).not.to.throw()
+    expect(() => {
+      tryParse('readonly')
+    }).not.to.throw()
+    expect(() => {
+      tryParse('params.readonly')
+    }).not.to.throw()
   })
 
   it('should throw an error vor invalid type syntax in any type', () => {
