7b13c6ba639c6a717e3fe3a331c4bd7b19859e0f
Brett Zamir <brettz9@yahoo.com>
2022-02-13 19:49:19 +0800

    test: imrpove catharsis transform coverage

---
+++ test/catharsisTransform.spec.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+
+import { RootResult } from '../src/result/RootResult'
+import { catharsisTransform } from '../src/transforms/catharsisTransform'
+
+describe('transform', () => {
+  it('Gets transform for suffixed `JsdocTypeVariadic`', () => {
+    const expected = {
+      name: 'B(c...)',
+      type: 'NameExpression'
+    }
+    const parseResult = {
+      type: 'JsdocTypeSymbol',
+      value: 'B',
+      element: {
+        type: 'JsdocTypeVariadic',
+        element: {
+          type: 'JsdocTypeName',
+          value: 'c'
+        },
+        meta: {
+          position: 'suffix',
+          squareBrackets: false
+        }
+      }
+    }
+    const xform = catharsisTransform(parseResult as RootResult)
+    expect(xform).to.deep.equal(expected)
+  })
+
+  it('Gets transform for suffixed `JsdocTypeVariadic`', () => {
+    const parseResult = {
+      type: 'JsdocTypeVariadic',
+      meta: {
+        squareBrackets: false
+      }
+    }
+    expect(() => {
+      catharsisTransform(parseResult as RootResult)
+    }).to.throw('dots without value are not allowed in catharsis mode')
+  })
+})
