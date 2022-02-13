44f199d80120dbe496857f89978d7ffe98ae3b61
Brett Zamir <brettz9@yahoo.com>
2022-02-13 19:23:14 +0800

    - test: complete coverage for `errors`

--- test/Parser.spec.ts
+++ test/Parser.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai'
 import { Parser } from '../src/Parser'
 
+import { NoParsletFoundError, EarlyEndOfParseError } from '../src/errors'
+
 import { typescriptGrammar } from '../src/grammars/typescriptGrammar'
 
 describe('Parser', () => {
@@ -15,5 +17,42 @@ describe('Parser', () => {
 
     expect(twoTokens).to.equal(false)
     expect(finalTokens).to.equal(true)
+  })
+
+  it('should return token of error with `NoParsletFoundError.getToken`', () => {
+    const parser = new Parser({
+      grammar: typescriptGrammar
+    })
+
+    let error
+    try {
+      parser.parseText('{')
+    } catch (err) {
+      error = err
+    }
+
+    if (error === undefined) {
+      throw new Error('Failed')
+    }
+
+    const token = (error as NoParsletFoundError).getToken()
+
+    expect(token).to.deep.equal({
+      type: 'EOF',
+      text: ''
+    })
+  })
+
+  it('should return token of error with `EarlyEndOfParseError.getToken`', () => {
+    const error = new EarlyEndOfParseError({
+      type: 'import',
+      text: 'import'
+    })
+    const token = error.getToken()
+
+    expect(token).to.deep.equal({
+      type: 'import',
+      text: 'import'
+    })
   })
 })
