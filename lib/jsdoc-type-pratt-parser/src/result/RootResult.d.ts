1c1f43e29e16c407daf4e77ce040375a712f43be
brettz9 <brettz9@yahoo.com>
2025-08-15 01:09:44 +0000

    Deploying to gh-pages from @ jsdoc-type-pratt-parser/jsdoc-type-pratt-parser@6ffcdc6d9964b82a3ba99c332535034e28897a08 🚀

--- lib/jsdoc-type-pratt-parser/src/result/RootResult.d.ts
+++ lib/jsdoc-type-pratt-parser/src/result/RootResult.d.ts
@@ -2,7 +2,7 @@ import { type JsdocObjectFieldResult, type KeyValueResult, type ObjectFieldResul
 /**
  * A parse result that corresponds to a valid type expression.
  */
-export type RootResult = NameResult | UnionResult | GenericResult | StringValueResult | NullResult | UndefinedResult | AnyResult | UnknownResult | FunctionResult | ObjectResult | NamePathResult | SymbolResult | TypeOfResult | KeyOfResult | ImportResult | TupleResult | SpecialNamePath | OptionalResult<RootResult> | NullableResult<RootResult> | NotNullableResult<RootResult> | VariadicResult<RootResult> | ParenthesisResult | IntersectionResult | NumberResult | PredicateResult | AssertsResult | ReadonlyArrayResult | AssertsPlainResult;
+export type RootResult = NameResult | UnionResult | GenericResult | StringValueResult | NullResult | UndefinedResult | AnyResult | UnknownResult | FunctionResult | ObjectResult | NamePathResult | SymbolResult | TypeOfResult | KeyOfResult | ImportResult | TupleResult | SpecialNamePath | OptionalResult<RootResult> | NullableResult<RootResult> | NotNullableResult<RootResult> | VariadicResult<RootResult> | ParenthesisResult | IntersectionResult | NumberResult | PredicateResult | AssertsResult | ReadonlyArrayResult | AssertsPlainResult | ConditionalResult;
 export type QuoteStyle = 'single' | 'double';
 /**
  * `element` is optional.
@@ -72,6 +72,7 @@ export interface GenericResult {
     type: 'JsdocTypeGeneric';
     left: RootResult;
     elements: RootResult[];
+    infer?: boolean;
     meta: {
         brackets: 'angle' | 'square';
         dot: boolean;
@@ -247,4 +248,14 @@ export interface ReadonlyArrayResult {
 export interface AssertsPlainResult {
     type: 'JsdocTypeAssertsPlain';
     element: NameResult;
+}
+/**
+ * A TypeScript conditional. Is used like this: `A extends B ? C : D`.
+ */
+export interface ConditionalResult {
+    type: 'JsdocTypeConditional';
+    checksType: RootResult;
+    extendsType: RootResult;
+    trueType: RootResult;
+    falseType: RootResult;
 }
